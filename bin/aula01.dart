95da498bd35b21ae943925e276c024def16913a6
Marcus Marques <marcusnopb@gmail.com>
2024-07-25 14:51:50 -0300

    primeiro contato

---
+++ bin/aula01.dart
@@ -0,0 +1,25 @@
+void main() {
+
+    //NÚMEROS
+    int idade = 23;
+    //int idade = 0x000017; 0x000017 == 23
+    print(idade);
+
+    double peso= 60.2;
+    //double altura = 175e6; 175e6 == 175000000
+    print(peso);
+
+    //BOoLEANOS
+    bool geek = true;
+    print(geek);
+
+    //bool teste = (idade != peso);
+    //print(teste);
+
+    //Palavras
+    String nome = 'Lox';
+    print('nome = $nome \n'
+        'idade = $idade anos\n'
+        'peso = $peso kgs' );
+
+}
